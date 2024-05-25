3a356349fcb12dff4f294dbce1ebedbd6737a882
afran18 <afran.vk18@gmail.com>
2024-05-25 23:14:51 +0530

    Food App 1st commit

---
+++ FOOD-ORDER/src/store/CartContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+    items:[],
+    addItems: (item) => {},
+    removeItem: (id) => {}
+});
+
+function cartReducer(state, action) {
+    if(action.type === 'ADD_ITEM') {
+        state.items.push(action.push);
+    }
+
+    if(action.type === 'REMOVE_ITEM') {
+
+    }
+
+    return state;
+}
+
+export function CartContextProvider( {children} ) {
+     useReducer(cartReducer, { items: [] });
+
+
+    return <CartContext.Provider>{children}</CartContext.Provider>
+}
+
+export default CartContext;
---
+++ FOOD-ORDER/src/util/formatting.js
@@ -0,0 +1,6 @@
+export const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'INR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+});
