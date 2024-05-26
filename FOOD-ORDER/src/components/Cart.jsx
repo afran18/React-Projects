34ea79e62f027f45c4b993197fe276658167e418
afran18 <afran.vk18@gmail.com>
2024-05-26 22:23:13 +0530

    food FE done

--- FOOD-ORDER/src/components/Cart.jsx
+++ FOOD-ORDER/src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from '../store/CartContext'
 import { currencyFormatter } from '../util/formatting';
 import Button from './UI/Button';
 import UserProgressContext from '../store/userProgressContext';
+import CartItem from './CartItem';
 
 function Cart() {
     const cartCtx = useContext(CartContext);
@@ -18,21 +19,42 @@ function Cart() {
         userProgressCtx.hideCart();
     }
 
+    const handleGoToCheckout = () => {
+        userProgressCtx.showCheckout();
+    }
+
   return (
-    <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+    <Modal className="cart"
+    open={userProgressCtx.progress === 'cart'} 
+    onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}
+    >
         <h2>You Cart</h2>
         <ul>
             {cartCtx.items.map( (item) => (
-            <li key={item.id}>
-                {item.name} - {item.quantity}
-            </li> 
+            <CartItem key={item.id} {...item} onIncrease={() => cartCtx.addItem(item)} onDecrease={ () => cartCtx.removeItem(item.id)} /> 
             ))}
         </ul>
+
+        
+        {/* <ul>
+            {cartCtx.items.map( (item) => (
+            <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={ () => cartCtx.addItem(item) }
+            onDecrease={ () => cartCtx.removeItem(id)}
+
+            /> 
+            ))}
+        </ul> */}
+        
 
         <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
         <p className="modal-actions">
             <Button textOnly onClick={handleCloseCart}>Close</Button>
-            <Button onClick={handleCloseCart}>Go to Checkout</Button>
+            {cartCtx.items.length > 0 && (<Button onClick={handleGoToCheckout}>Go to Checkout</Button>)}
         </p>
     </Modal>
   )
